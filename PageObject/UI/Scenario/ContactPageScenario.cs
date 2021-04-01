38e1fd7499e24b1018475042503de78bfae7e025
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-04-02 02:32:20 +0300

    code style

--- PageObject/UI/Scenario/ContactPageScenario.cs
+++ PageObject/UI/Scenario/ContactPageScenario.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PageObject
+﻿namespace PageObject
 {
     public class ContactPageScenario
     {
-        public void CallMeBackTest()
+        public void CallMeBack()
         {
             var page = new PageBuilder()
                 .SetBrowser(BrowserType.Firefox)
