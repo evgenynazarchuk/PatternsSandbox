d56fa453429d43b9456475f16049c5bdf280883c
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-04-30 20:50:32 +0300

    add result

---
+++ ResultPattern/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ResultPattern
+{
+    class Program
+    {
+        static void Main()
+        {
+            //
+            Result<int> result1 = GetData1();
+            Result<int> result2 = GetData2();
+
+            //
+            if (result1.Success)
+            {
+                Console.WriteLine(result1.Value);
+            }
+            else if(result1.Failure)
+            {
+                Console.WriteLine(result1.ErrorMessage);
+            }
+
+            //
+            if (result2.Success)
+            {
+                Console.WriteLine(result2.Value);
+            }
+            else if (result2.Failure)
+            {
+                Console.WriteLine(result2.ErrorMessage);
+            }
+        }
+
+        public static Result<int> GetData1()
+        {
+            return new Result<int>(42);
+        }
+
+        public static Result<int> GetData2()
+        {
+            return new Result<int>("Answer");
+        }
+    }
+}
---
+++ ResultPattern/Result.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ResultPattern
+{
+    public class Result<TResult>
+    {
+        public TResult Value { get; set; }
+        public string ErrorMessage { get; set; }
+        public Exception Exception { get; set; } = default;
+
+        public bool Success { get => _success; set => _success = value; }
+        public bool Failure { get => !_success; set => _success = !value; }
+
+        private bool _success;
+
+        public Result(TResult value)
+        {
+            this.ErrorMessage = string.Empty;
+            this.Exception = default;
+            this.Value = value;
+            this._success = true;
+        }
+
+        public Result(string errorMessage)
+        {
+            this.ErrorMessage = errorMessage;
+            this.Exception = new Exception(errorMessage);
+            this._success = false;
+        }
+
+        public Result(Exception exception)
+        {
+            this.ErrorMessage = exception.Message;
+            this.Exception = exception;
+            this._success = false;
+        }
+
+        public Result(string errorMessage, Exception exception)
+        {
+            this.ErrorMessage = errorMessage;
+            this.Exception = exception;
+            this._success = false;
+        }
+    }
+}
