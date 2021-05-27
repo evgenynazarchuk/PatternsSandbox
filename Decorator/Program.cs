2f669a11c9e6f2dd2bbd1df0825664d54d333993
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-05-28 01:11:04 +0300

    add description

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -6,6 +6,9 @@ namespace Decorator
     {
         static void Main()
         {
+            // класс декоратор и функциональный класс реализуют идентичный интерфейс
+            // класс декоратор содерт функциональный класс для перехвата вызова
+            // перехватываюмую функцию/метод возможно следуюет реализовать в виде отдельного интерфейса
             var messages = new List<IMessage>()
             {
                 new YellowMessage(new DoubleBeepMessage("Hello world")),
